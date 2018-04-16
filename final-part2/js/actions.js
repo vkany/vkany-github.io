04085f1434ebc7ea3ce0e341e7da6869896e1d25
Vibhuti Kanitkar <vibhuti@umich.edu>
2018-04-16 11:56:35 -0400

    push new changes

--- final-part2/js/actions.js
+++ final-part2/js/actions.js
@@ -1,21 +1,7 @@
 
-$(function hide() {
-    var image1 = document.getElementById("image1");
-    var hide  = document.getElementById('hide');
-    hide.onclick=image1.style.display = "none";
-});
-
-$(function show() {
-    var image1 = document.getElementById("image1");
-    var show = document.getElementById('show');
-    show.onclick = image1.style.display = "block";
-});
-
-
-$("#hide").click(function(){
-    $("image1").hide();
-});
-
-$("#show").click(function(){
-    $("image1").show();
-});
+function hide() {
+  document.getElementById("image1").style.display = "none";
+}
+function show() {
+document.getElementById("image1").style.display = "block";
+}
